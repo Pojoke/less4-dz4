265a099a138073d78c543eac767f2f164c85bd35
Pojoke <pavlykdmutro@gmail.com>
2024-09-19 14:23:47 +0300

    good

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$page_name = "Loop";
+$header = <<<EOD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$page_name</title>
+
+</head>
+<body>\n
+EOD;
+$footer = <<<EOD
+</body>
+</html>
+EOD;
+
+// echo $header;
+
+// for ($i=1; $i <= 6; $i++) {
+//     echo "  <h$i>$page_name $i</h$i>\n";
+// }
+
+echo "<table>";
+for ($i = 1; $i <= 6; $i++) {
+    echo "  <tr><td>$i</td><td>  <img src='./images/close-up-felt-animal.jpg' alt='' width='100px'> </td></tr>\n";
+}
+echo "</table>\n";
+
+echo $footer;
